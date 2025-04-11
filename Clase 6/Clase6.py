87a107ef90f97db098c1e43069087622c7160ed6
Gonzalo-6 <g-arranz@hotmail.com>
2025-04-11 12:50:23 +0200

    dia 1/4

---
+++ Clase 6/Clase6.py
@@ -0,0 +1,18 @@
+#mi_Matriz = [[1, 2,3],
+ #            [4, 5, 6],
+  #           [7, 8, 9]]
+#mi_Matriz[2][1]=99
+
+#tuplas =((1,2),(3,4), [5,6])
+
+#print(tuplas)
+#matriz [fila][columna]
+
+mi_lista = [1,2,3]
+#mi_lista.insert(1,25)
+
+#mi_lista.remove(3)
+#mi_lista.pop(0)
+#mi_lista.reverse()
+mi_lista.sort()
+print(mi_lista)
